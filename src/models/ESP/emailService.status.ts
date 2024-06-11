ff6df7e64ff75789e95d5422b3e4a4d4d9d3877a
Romain (Mac Sudio) <romain@demoustier.com>
2024-06-11 16:22:09 +0200

    New status for email-service

--- src/models/ESP/emailService.status.ts
+++ src/models/ESP/emailService.status.ts
@@ -1,9 +1,9 @@
 import { ESPStandardizedWebHook } from "../../types/error.type";
 
 export const webHookStatus : { [key: string]: ESPStandardizedWebHook } = {
-	DELIVERY: {status : 'DELIVERY', message: 'Message sent'},
-	BOUNCE:{status : 'BOUNCE', message: 'Message bounced'},
-	SPAM:{status : 'SPAM', message: 'Message marked as spam'},
-	OPEN:{status : 'OPEN', message: 'Message opened'},
-	LINK:{status : 'LINK_CLICK', message: 'Link clicked'}
+	DELIVERY: {webHookType : 'DELIVERY', message: 'Message sent'},
+	BOUNCE:{webHookType : 'BOUNCE', message: 'Message bounced'},
+	SPAM:{webHookType : 'SPAM', message: 'Message marked as spam'},
+	OPEN:{webHookType : 'OPEN', message: 'Message opened'},
+	LINK:{webHookType : 'LINK_CLICK', message: 'Link clicked'}
 }
