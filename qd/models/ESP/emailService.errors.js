46ef6a557858c4b3cc80b23d7bcedfb3d281ef14
Romain (Mac Sudio) <romain@demoustier.com>
2025-01-30 19:05:23 +0100

    New folder for qd

--- qd/models/ESP/emailService.errors.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorCode = void 0;
-exports.errorCode = {
-//TO DO: Add error codes
-};
--- qd/utils/transformeHeaders.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.transformHeaders = transformHeaders;
-function transformHeaders(headers) {
-    return headers.reduce((acc, header) => {
-        acc[header.name] = header.value;
-        return acc;
-    }, {});
-}
